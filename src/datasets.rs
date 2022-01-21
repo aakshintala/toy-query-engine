d266d493d38a3df7e6009bec962cb4b56f4139b8
Amogh Akshintala <aakshintala@cs.stonybrook.edu>
2022-01-21 18:10:47 -0500

    Add doc-comments to dataset.rs

--- src/datasets.rs
+++ src/datasets.rs
@@ -3,15 +3,30 @@ use std::fmt::Display;
 
 use serde::Deserialize;
 
-#[derive(Debug, Deserialize)]
+/// In-memory representation of each record in the `country.csv` dataset.
+/// This is represented as a struct so we can use the [`serde`] and [`csv`] crates to generate
+/// the deserialization code.
+///
+/// Example record:
+/// CountryCode, CountryName, Continent,        CountryPop, Capital
+/// ABW,         Aruba,       North_America,    103000,     129
+#[derive(Clone, Debug, Deserialize)]
+// This is necessary as the header in the dataset (`country.csv`) is in CamelCase. `serde` and `csv`
+// rely on these names being the same as those in the header row in the dataset.
 #[allow(non_snake_case)]
 pub struct Country {
+    /// "ABW" in the example above.
     pub CountryCode: String,
+    /// "Aruba" in the example above.
     pub CountryName: String,
+    /// "North_America" in the example above.
     pub Continent: String,
+    /// 103000 in the example above.
     pub CountryPop: i64,
+    /// 129 in the example above.
     pub Capital: Option<i64>,
 }
+
 impl Display for Country {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         let capital = if self.Capital.is_some() {
@@ -26,6 +41,11 @@ impl Display for Country {
     }
 }
 
+/// Helper function to deserialize the `country.csv` dataset.
+///
+/// Returns
+/// A vector of all the rows in the dataset represented as a [`Country`], or
+/// an error propagated from the csv and serde deserialization code.
 fn load_countries() -> Result<Vec<Country>, Box<dyn Error>> {
     let mut countries: Vec<Country> = Vec::new();
     let mut csv_reader = csv::Reader::from_path("data/country.csv")?;
@@ -36,12 +56,25 @@ fn load_countries() -> Result<Vec<Country>, Box<dyn Error>> {
     Ok(countries)
 }
 
-#[derive(Debug, Deserialize)]
+/// In-memory representation of each record in the `city.csv` dataset.
+/// This is represented as a struct so we can use the [`serde`] and [`csv`] crates to generate
+/// the deserialization code.
+///
+/// Example record:
+/// CityID, CityName,   CountryCode,    CityPop
+/// 1,      Kabul,      AFG,            1780000
+#[derive(Clone, Debug, Deserialize)]
+// This is necessary as the header row in the dataset (`city.csv`) is in CamelCase. `serde` and
+// `csv` rely on these names being the same as those in the header row in the dataset.
 #[allow(non_snake_case)]
 pub struct City {
+    /// 1 in the example above.
     pub CityID: i32,
+    /// "Kabul" in the example above.
     pub CityName: String,
+    /// "AFG" in the example above.
     pub CountryCode: String,
+    /// 1780000 in the example above.
     pub CityPop: i64,
 }
 
@@ -54,6 +87,11 @@ impl Display for City {
     }
 }
 
+/// Helper function to deserialize the `city.csv` dataset.
+///
+/// Returns
+/// A vector of all the rows in the dataset represented as a [`City`], or
+/// an error propagated from the csv and serde deserialization code.
 fn load_cities() -> Result<Vec<City>, Box<dyn Error>> {
     let mut cities: Vec<City> = Vec::new();
     let mut csv_reader = csv::Reader::from_path("data/city.csv")?;
@@ -64,10 +102,21 @@ fn load_cities() -> Result<Vec<City>, Box<dyn Error>> {
     Ok(cities)
 }
 
-#[derive(Debug, Deserialize)]
+/// In-memory representation of each record in the `city.csv` dataset.
+/// This is represented as a struct so we can use the [`serde`] and [`csv`] crates to generate
+/// the deserialization code.
+///
+/// Example record:
+/// CountryCode,    Language
+/// ABW,            Dutch
+#[derive(Clone, Debug, Deserialize)]
+// This is necessary as the header row in the dataset (`city.csv`) is in CamelCase. `serde` and
+// `csv` rely on these names being the same as those in the header row in the dataset.
 #[allow(non_snake_case)]
 pub struct Language {
+    /// "ABW" in the example above.
     pub CountryCode: String,
+    /// "Dutch" in the example above.
     pub Language: String,
 }
 
@@ -77,6 +126,11 @@ impl Display for Language {
     }
 }
 
+/// Helper function to deserialize the `language.csv` dataset.
+///
+/// Returns
+/// A vector of all the rows in the dataset represented as a [`Language`], or
+/// an error propagated from the csv and serde deserialization code.
 fn load_languages() -> Result<Vec<Language>, Box<dyn Error>> {
     let mut languages: Vec<Language> = Vec::new();
     let mut csv_reader = csv::Reader::from_path("data/language.csv")?;
@@ -87,11 +141,14 @@ fn load_languages() -> Result<Vec<Language>, Box<dyn Error>> {
     Ok(languages)
 }
 
-#[allow(dead_code)]
-#[derive(Debug)]
+/// In-memory representation of all the loaded datasets.
+#[derive(Clone, Debug)]
 pub struct DataSet {
+    /// All the rows in the `country.csv` dataset.
     pub countries: Vec<Country>,
+    /// All the rows in the `city.csv` dataset.
     pub cities: Vec<City>,
+    /// All the rows in the `language.csv` dataset.
     pub languages: Vec<Language>,
 }
 
